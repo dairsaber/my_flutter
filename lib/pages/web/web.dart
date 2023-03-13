a6b5a424f8e09dfb0d53e7d7720d9aca65d5aba9
dairsaber <dairsaber@outlook.com>
2023-03-13 15:59:09 +0800

    feat: 初步实现flutter webview

---
+++ lib/pages/web/web.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+import 'package:webview_flutter/webview_flutter.dart';
+// #docregion platform_imports
+// Import for Android features.
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+// IOS
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+import 'navigation_controller.dart';
+
+class Web extends StatefulWidget {
+  const Web({Key? key}) : super(key: key);
+
+  @override
+  State<Web> createState() => _WebState();
+}
+
+class _WebState extends State<Web> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // #docregion platform_features
+    late final PlatformWebViewControllerCreationParams params;
+
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+    // #enddocregion platform_features
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            debugPrint('WebView is loading (progress : $progress%)');
+          },
+          onPageStarted: (String url) {
+            debugPrint('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            debugPrint('Page finished loading: $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            printErr(error);
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+
+            // TODO 这边后期可以做一些url地址的拦截
+            // if (request.url.startsWith('https://www.youtube.com/')) {
+            //   debugPrint('blocking navigation to ${request.url}');
+            //   return NavigationDecision.prevent;
+            // }
+            // debugPrint('allowing navigation to ${request.url}');
+            // return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..addJavaScriptChannel(
+        'Toaster',
+        onMessageReceived: (JavaScriptMessage message) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        },
+      )
+      ..loadRequest(Uri.parse('http://192.168.110.110:5176'));
+
+    // #docregion platform_features
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+    // #enddocregion platform_features
+
+    _controller = controller;
+  }
+
+  Future<bool> onWillPop() async {
+    // 调用javascript 中的路由pop
+    var isOk = await _controller
+        .runJavaScriptReturningResult('___Flutter.routerPop()');
+
+    debugPrint("系统级别的后退$isOk");
+    return isOk as bool;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: onWillPop,
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: SafeArea(child: WebViewWidget(controller: _controller)),
+        ));
+  }
+}
+
+// ====工具函数====
+void printErr(WebResourceError error) {
+  debugPrint('''
+Page resource error:
+  code: ${error.errorCode}
+  description: ${error.description}
+  errorType: ${error.errorType}
+  isForMainFrame: ${error.isForMainFrame}
+          ''');
+}
